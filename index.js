fcaf7ab63588245e133ad1b3acfc2ae6dcec8ab5
jschwartzmtn <jschwartz@mountainproductions.com>
2016-04-25 10:42:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://admin:M0unta1n@ds064748.mlab.com:64748/portfolio',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'GCBfwawY7a4MRekcHuAgDkhVyaJG2Q4v69xsRJLh',
   masterKey: process.env.MASTER_KEY || 'fNX9VrkcTMjM7ApRrptic3fhXn6oIudaqWITPzwn', //Add your master key here. Keep it secret!
